7c9d368c4fbf516ce11354761bdc2d03e1af38e1
onisuly <onisuly@gmail.com>
2018-08-14 00:39:36 +0800

    Update shell.

--- startup.sh
+++ startup.sh
@@ -8,12 +8,14 @@ if [ $SECURE = 'true' ]; then
         rm -f /usr/local/nginx/.htpasswd
     fi
     htpasswd -b -c /usr/local/nginx/.htpasswd ${USERNAME} ${PASSWORD}
+    cp /usr/local/nginx/conf/nginx.conf /usr/local/nginx/conf/nginx.conf.bak
 fi
 
 if [ -f /usr/local/nginx/.htpasswd ]; then
-    if ! grep -Fxq "auth_basic \"Restricted Content\";" usr/local/nginx/conf/nginx.conf; then
-        sed -i '/google_scholar on;/ a\            auth_basic "Restricted Content";\n            auth_basic_user_file /usr/local/nginx/.htpasswd;' usr/local/nginx/conf/nginx.conf
+    if [ -f /usr/local/nginx/conf/nginx.conf.bak ]; then
+        cp /usr/local/nginx/conf/nginx.conf.bak /usr/local/nginx/conf/nginx.conf
     fi
+    sed -i '/google_scholar on;/ a\            auth_basic "Restricted Content";\n            auth_basic_user_file /usr/local/nginx/.htpasswd;' /usr/local/nginx/conf/nginx.conf
 fi
 
 /usr/local/nginx/sbin/nginx
